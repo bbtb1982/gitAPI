b91b3e708e67ce52560dd236ad581bd07eac1788
Brandon Richards <bbtb1982@gmail.com>
2016-01-17 01:36:20 -0700

    add lodash, update gitignore

--- es6/main.js
+++ es6/main.js
@@ -1,3 +1,13 @@
 import Point from './Point.js';
+import lodash from 'lodash';
+
+var users = [
+  { 'user': 'barney',  'age': 36, 'active': true },
+  { 'user': 'fred',    'age': 40, 'active': false },
+  { 'user': 'pebbles', 'age': 1,  'active': true }
+];
+
+var first = _.find(users, 'active');
+
 var body = document.querySelector('body');
-body.textContent = 'Good point: ' + new Point(1, 23);
+body.textContent = 'Good point: ' + first.age;
